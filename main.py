1935b28d94c7ec44aa3895985fa340523e0163dd
Paul <muenchwolfpaul@gmail.com>
2021-03-19 13:49:33 +0100

    Sercise 4,5
    i know it took long, i had to cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-
 # Exercises 6.15
+
+from turtle import *
 
 def konkat(*args    # collection of strings
            ):
@@ -53,3 +54,78 @@ def wurzel(x,       # to be square rooted (Integer)
     else:
         return wurzel(x, n-1, 0.5*(z + x/z))
 # print(wurzel(144))
+
+def bewege(n,           # amount of disks to be moved
+           von,         # stating position
+           nach,        # end position
+           ueber,       # support position
+           ):
+    """
+    Solves the Problem of Hanoi
+
+    Requirements: No smaller disks are distributed on the end/support position
+    prints out a list of iterative movement commands, no return
+    Paul Münch 19.March 2021
+    """
+    if n == 1:
+        print("Lege eine Scheibe von ", von, " nach ", nach)
+    else:
+        bewege(n-1, von, ueber, nach)
+        bewege(1, von, nach, ueber)
+        bewege(n-1, ueber, nach, von)
+# bewege(3, 1, 2, 3)
+
+def create_tree(n=1):
+    """
+    Creates a tree in Logo
+
+    :param n: Variant of the tree 1-3 (Integer)
+    """
+
+    if n == 1:
+        def baum(x):
+            if x < 5:
+                return
+            else:
+                forward(x)
+                left(45)
+                baum(x/2)
+                right(90)
+                baum(x/2)
+                left(45)
+                back(x)
+            return
+
+    elif n == 2:
+        def baum(x):
+            if x < 3:
+                return
+            else:
+                forward(x)
+                left(25)
+                baum(x*3/4)
+                right(90)
+                baum(x/2)
+                left(65)
+                back(x)
+            return
+
+    elif n == 3:
+        def baum(x):
+            if x < 3:
+                return
+            else:
+                forward(x)
+                left(90)
+                baum(x * 2/3)
+                right(180)
+                baum(x * 2/3)
+                left(90)
+                back(x)
+            return
+
+    left(90)
+    baum(100)
+    hideturtle()
+    input()
+# create_tree(3)
